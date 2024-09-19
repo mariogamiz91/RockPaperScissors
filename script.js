ea0564cdb31a576f32e47d6244a5aaaa02420de5
mariogamiz <mario.gamizavello@gmail.com>
2024-09-18 23:16:02 -0600

    All the logic in the game was added, the game is setup to the first in win 5 rounds, it display the winner, remove all the content and add a new game button that refresh the page, all with dom manipulation, still need to add a nice UI to play the game

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+const buttons = document.querySelectorAll("button");
+const playerWonText= "You have won this round";
+const computerWonText= "You loose, skynet has won this round";
+const tieText = "It's a tie"
+var playerScore = 0;
+var computerScore = 0;
+var tieScore = 0;
+const buttonsGame = document.querySelector(".buttons-section");
+const choicesSection = document.querySelector(".choices-section");
+const scoreSection = document.querySelector(".score-section");
+
+const newGameButton = document.createElement('button');
+newGameButton.textContent = 'New game!';
+
+
+
+
+var winner = document.getElementById("winner");
+
+
 function getComputerChoice (){
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0){
@@ -6,7 +27,7 @@ function getComputerChoice (){
         return 'paper'
     } else if(choice==2) {
         return 'scissors'
-    }    
+    }   
 }
 
 function getHumanChoice(){
@@ -16,43 +37,95 @@ function getHumanChoice(){
 
 
 
-const buttons = document.querySelectorAll("button");
+
+
+
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
   // and for each one we add a 'click' listener
   button.addEventListener("click", () => {
-    let computerPick = getComputerChoice();
-    alert(button.value);
-    alert(computerPick);
-        
-        if (computerPick == 'rock' && button.value == 'rock'){
-            alert("It's a Tie");
-    }   else if (computerPick == 'rock' && button.value == 'paper'){
-            alert("You human won");
-    }   else if (computerPick == 'rock' && button.value=='scissors'){
-            alert("Computer has won");
-    }   else if (computerPick == 'paper' && button.value=='rock'){
-        alert("Computer has won");
-    }   else if (computerPick == 'paper' && button.value=='paper'){
-        alert("It's a Tie");
-    }   else if (computerPick == 'paper' && button.value=='scissors'){
-        alert("You human won");
-    }   else if (computerPick == 'scissors' && button.value=='rock'){
-        alert("You human won");
-    }   else if (computerPick == 'scissors' && button.value=='paper'){
-        alert("Computer has won");
-    }   else if (computerPick == 'scissors' && button.value=='scissors'){
-        alert("It's a Tie");
-    }
+    
 
+    let computerPick = getComputerChoice();
+    document.getElementById("computerResult").innerHTML = computerPick;
+    document.getElementById("humanResult").innerHTML = button.value;
+    
+           
+        if (computerPick == 'rock' && button.value == 'rock'){
+            document.getElementById("winner").innerHTML = tieText;
+            tieScore = tieScore + 1;
+            document.getElementById("tieScore").innerHTML = tieScore;
+
+    }   else if (computerPick == 'rock' && button.value == 'paper'){
+        document.getElementById("winner").innerHTML = playerWonText;
+        playerScore = playerScore + 1;
+        document.getElementById("playerScore").innerHTML = playerScore;
+    }   else if (computerPick == 'rock' && button.value=='scissors'){
+        document.getElementById("winner").innerHTML = computerWonText;
+        computerScore = computerScore + 1;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    }   else if (computerPick == 'paper' && button.value=='rock'){
+        document.getElementById("winner").innerHTML = computerWonText;
+        computerScore = computerScore + 1;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    }   else if (computerPick == 'paper' && button.value=='paper'){
+        document.getElementById("winner").innerHTML = tieText;
+        tieScore = tieScore + 1;
+        document.getElementById("tieScore").innerHTML = tieScore;
+    }   else if (computerPick == 'paper' && button.value=='scissors'){
+        document.getElementById("winner").innerHTML = playerWonText;
+        playerScore = playerScore + 1;
+        document.getElementById("playerScore").innerHTML = playerScore;
+    }   else if (computerPick == 'scissors' && button.value=='rock'){
+        document.getElementById("winner").innerHTML = playerWonText;
+        playerScore = playerScore + 1;
+        document.getElementById("playerScore").innerHTML = playerScore;
+    }   else if (computerPick == 'scissors' && button.value=='paper'){
+        document.getElementById("winner").innerHTML = computerWonText;
+        computerScore = computerScore + 1;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    }   else if (computerPick == 'scissors' && button.value=='scissors'){
+        document.getElementById("winner").innerHTML = tieText;
+        tieScore = tieScore + 1;
+        document.getElementById("tieScore").innerHTML = tieScore;
+    }   
+
+                //Validation of score
+                if (computerScore >= 5){
+                    document.getElementById("winner").innerHTML = "Skynet has won 5 rounds, you loosed the game, humanity is doom";
+                        buttonsGame.remove();
+                        choicesSection.remove();
+                        scoreSection .remove();
+                        winner.appendChild(newGameButton);
+                        newGameButton.addEventListener("click", newGame);
+                        
+                   
+
+                   
+
+                } else if (playerScore >=5){
+                    document.getElementById("winner").innerHTML = "You have won 5 rounds, humanity is safe against skynet"
+                        buttonsGame.remove();
+                        choicesSection.remove();
+                        scoreSection.remove();
+                        winner.appendChild(newGameButton);
+                        newGameButton.addEventListener("click", newGame);
+                      
+                    
+                 
+                }
+
+                       
 
 
     
   });
 });
 
-
+function newGame(){
+    location.reload();
+}
 
 
 
